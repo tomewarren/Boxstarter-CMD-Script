20a78aa98225c733b66b66cfb56746d7ac4ab68e
tomewarren <tomewarren@gmail.com>
2015-08-17 14:19:50 -0400

    recommit

--- tom-boxstarter.cmd
+++ tom-boxstarter.cmd
@@ -12,6 +12,7 @@ $Boxstarter.RebootOk=$true # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
+
 #====================================================
 # Basic Setup
 Update-ExecutionPolicy Unrestricted -Force
